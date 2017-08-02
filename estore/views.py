ce4b0ff654d89fe9a35422e32e6113f70629ca0c
ericaiei <ericayao@gmail.com>
2017-08-02 20:55:29 +0800

    Create backstage

---
+++ estore/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+
+# Create your views here.
+
+from .forms import ProductForm
+from .models import Product
+
+
+# Create your views here.
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'estore/product_list.html', {'products': products})
+
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'estore/product_form.html', {'form': form})
+
